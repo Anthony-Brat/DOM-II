6b394984118e91d5af2726c192748cbe74ce0a40
unknown <tonybrat74@gmail.com>
2020-04-07 14:30:17 -0700

    1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+// Your code goes here 
+// store html in variable
+const title = document.querySelector('h1')
+// when h1 is moused over text color will change to red
+// add event
+title.addEventListener('mouseover', function(event){
+    event.target.style.color = 'red'
+})
+
